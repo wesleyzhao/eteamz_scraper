3406a11843eeef50f5336323629cc8d623ec81f3
Wesley Zhao <wesley.zhao@gmail.com>
2011-09-14 17:41:33 -0700

    first commit

---
+++ eteamz_scraper.py
@@ -0,0 +1,68 @@
+from BeautifulSoup import BeautifulSoup
+from lxml.html import parse
+import urllib2,urllib
+import MySQLdb
+
+def get_cursor():
+    conn = MySQLdb.connect(host = '50.16.213.18', user = 'root', passwd = 'AwsOmPass123', db = 'eteamz')
+    cursor = conn.cursor()
+    return {'cursor': cursor, 'conn' : conn}
+
+def get_links(url):
+    doc = parse(url).getroot()
+    #main_table = doc.cssselect('div#search_results table')
+    result_links = [link.get('href') for link in doc.cssselect("div#search_results table a")] #create a list of urls from the anchor tags in table, then get the 'href' from that list
+    return result_links
+
+def make_url(page, search_box = "", city = "", state = "", sport = "", skill = "", age = "", gender = "", organization = ""):
+    base_url = "http://search.eteamz.com/search/"
+    params = {
+        'page': page,
+        'search_box' : search_box,
+        'city' : city,
+        'state' : state,
+        'sport' : sport,
+        'skill' : skill,
+        'age' : age,
+        'gender' : gender,
+        'organization' : organization,
+        }
+    params_url = urllib.urlencode(params)
+    return base_url + "?" + params_url
+        
+def insert_links(links, state, gender):
+    insert_str = ", ".join(["('%s', '%s', '%s')" % (link, state, gender) for link in links])
+    query_str = "INSERT INTO teams (url, state, gender) VALUES %s" % (insert_str)
+    mysql = get_cursor()
+    cur = mysql['cursor']
+    cur.execute(query_str)
+    mysql['conn'].commit()
+    return cur.rowcount
+
+def mark_stop(page_num, state, gender):
+    mysql = get_cursor()
+    cur = mysql['cursor']
+    cur.execute("INSERT INTO stops (page_number, state, gender) VALUES (%s, '%s', '%s')" % (str(page_num), state, gender))
+    mysql['conn'].commit()
+    return cur.rowcount
+
+def main():
+    states = [''] #already did Washington
+    genders = ['Female','Male','Coed']
+    for state in states:
+        #make sure it is done for every state
+        for gender in genders:
+            #make sure it is done for every gender
+            page_num =  1 #starting page
+            last_link_len = 10
+            while  last_link_len >=10:
+                url = make_url(page = page_num, gender = gender, state = state)
+                links = get_links(url)
+                last_link_len = len(links)
+                page_num += 1
+                if links:
+                    insert_links(links, state, gender)
+                print 'inserted -- url: %s,   gender: %s,   # of links %s' % (url, gender, str(last_link_len))
+            mark_stop(page_num - 1, state, gender)
+            print 'Stopped at page # %s, state: %s, gender %s' %(str(page_num-1), state, gender)
+
