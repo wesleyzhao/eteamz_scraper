4f6c080734d1e374327b2f5ec56a164194757464
Wesley Zhao <wesley.zhao@gmail.com>
2011-09-15 06:50:04 +0000

    fixed up for sport param

--- eteamz_scraper.py
+++ eteamz_scraper.py
@@ -3,12 +3,9 @@ from lxml.html import parse
 import urllib2,urllib
 import MySQLdb
 
-#states = ['Alabama', 'Alaska', 'Alberta', 'Arizona', 'Arkansas', 'British Columbia', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'International', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Manitoba', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Brunswick', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'Newfoundland', 'North Carolina', 'North Dakota', 'Northwest Territories', 'Nova Scotia', 'Ohio', 'Oklahoma', 'Ontario', 'Oregon', 'Pennsylvania', 'Prince Edward Island', 'Puerto Rico', 'Quebec', 'Rhode Island', 'Saskatchewan', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'West Virginia', 'Wisconsin', 'Wyoming', 'Yukon'] #already did Washington
-states = ['Alabama']
-#genders = ['Female','Male','Coed']
-genders = ['Coed']
+states = ['Alaska', 'Alberta', 'Arizona', 'Arkansas', 'British Columbia', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'International', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Manitoba', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Brunswick', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'Newfoundland', 'North Carolina', 'North Dakota', 'Northwest Territories', 'Nova Scotia', 'Ohio', 'Oklahoma', 'Ontario', 'Oregon', 'Pennsylvania', 'Prince Edward Island', 'Puerto Rico', 'Quebec', 'Rhode Island', 'Saskatchewan', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'West Virginia', 'Wisconsin', 'Wyoming', 'Yukon'] #already did Washington,Alabama
+genders = ['Female','Male','Coed']
 ages = ['All Ages', 'Adult', 'Freshman', 'Jr Varsity', 'Senior', 'Sophomore', 'Varsity', 'Youth', 'Youth 10u', 'Youth 11u', 'Youth 12u', 'Youth 13u', 'Youth 14u', 'Youth 15u', 'Youth 16u', 'Youth 17u', 'Youth 18u', 'Youth 6u', 'Youth 7u', 'Youth 8u', 'Youth 9u']
-#ages = ['Youth 6u', 'Youth 7u', 'Youth 8u', 'Youth 9u']
 sports = ['Action Sports', 'Adventure Racing', 'Aerobics', 'Archery', 'Badminton', 'Band', 'Bandy', 'Baseball', 'Baseball - Little League', 'Baseball FED Rules', 'Baseball Rules', 'Basketball', 'Baton Twirling', 'Biathlon', 'Billiards', 'Bobsleigh', 'Bocce', 'Body Building', 'Bowling', 'Boxing', 'Broomball', 'Canoeing', 'Cheerleading', 'Chess', 'Climbing', 'Company', 'Cricket', 'Croquet', 'Cross Country', 'Curling', 'Cycling', 'Dance', 'Darts', 'Disc Golf', 'Disc Sports', 'Diving', 'Dodgeball', 'Duathlon', 'Enduro and RAID Rally', 'Equestrian', 'Fencing', 'Field Hockey', 'Figure Skating', 'Fire', 'Fishing', 'Fitness', 'Flying-R/C', 'Foosball', 'Football', 'Football-AU', 'Football-Austrailian Rules', 'Football-Flag', 'Golf', 'Gymnastics', 'Handball', 'Handball-Team', 'Hockey-Ball', 'Hockey-Ice', 'Hockey-Inline', 'Hockey-Street', 'Horseshoes', 'Hunting', 'Ice Skating', 'Inline Skating', 'Judo', 'Jump Rope', 'Karate', 'Kayak', 'Kickball', 'Kite Club', 'Lacrosse', 'Luge', 'Lumberjacking', 'Marching Drill Team/Drum Lines', 'Martial Arts', 'Motocross', 'Motorsport', 'Mountain Biking', 'Multi-Sport', 'Myteam', 'Netball', 'Orienteering', 'Other', 'Other Sports', 'Outdoors', 'Outrigging', 'Paddleball', 'Paintball', 'Police', 'Polo', 'Poms', 'Quoit', 'Racewalking', 'Racing', 'Racing-Dragon Boat', 'Racing-Horse', 'Racing-Kart', 'Racquetball', 'Rafting', 'Ringette', 'Rodeo', 'Rowing', 'Rugby', 'Running', 'Sailing', 'Scouting', 'Scuba Diving', 'Sheriff', 'Shooting', 'Skateboarding', 'Skating', 'Skating-Speed', 'Skating-Synchro', 'Skiing', 'Skydiving', 'Skysurfing', 'Snorkeling', 'Snow Boarding', 'Soccer', 'Softball', 'Softball-Fastpitch', 'Softball-Slowpitch', 'Spinning', 'Squash', 'Step Teams', 'Stickball', 'Strength & Conditioning', 'Support', 'Surfing', 'Swimming', 'Swimming-Synchro', 'Tae Kwon Do', 'Tee Ball', 'Tennis', 'Tennis-Platform', 'Tennis-Table', 'Track & Field', 'Tractor Pulling', 'Triathlon', 'Ultimate Frisbee', 'Volleyball', 'Wakeboarding', 'Walking', 'Wally Ball', 'Water Polo', 'Water Skiing', 'Weight Lifting', 'Whiffle Ball', 'Windsurfing', 'Winter Sports', 'Wrestling', 'Yoga']
 skills = ['Church', 'Club', 'College', 'Competitive', 'Corporate', 'Elementary', 'High School', 'Jr College', 'Middle School', 'National', 'Other', 'Professional', 'Recreational', 'Semi-Pro', 'Tournament']
 organizations = ['AA Youth Basketball', 'American Amateur Baseball Congress', 'American Association of Cheerleading Coaches and Advisors', 'Amateur Athletic Union Boys Basketball', 'Amateur Athletic Union Girls Basketball', 'Amateur Athletic Union Jr. Golf', 'American Amateur Youth Baseball Alliance', 'All American Youth Football & Cheer', 'AFA Flag Football', 'AFA Semi-Pro', 'American Legion Baseball', 'American Running Association', 'American Roundball Corporation', 'Amateur Softball Association', 'American Youth Soccer Organization', 'Babe Ruth League Baseball', 'Basketball Congress International', 'British Youth American Football Association', 'Continental Amateur Baseball Association', 'Cal Ripken League', 'Connie Mack World Series', 'Central Texas Fastbreak Basketball', 'Dixie Youth Baseball', 'Dixie Softball', 'Dizzy Dean Baseball', 'Babe Ruth League Softball', 'Georgia Youth Football Conference', 'Indian Nations Youth Football', 'International Senior Softball Association', 'Little League Baseball and Softball', 'Mickey Mantle Baseball', 'Mid-American Youth Basketball', 'Mens Senior/Adult Baseball League', 'National Adult Baseball Association', 'National Amateur Baseball Federation', 'National Baseball Congress', 'National Field Hockey Coaches Association', 'National Softball Association', 'Pony Baseball', 'Pony', 'Pop Warner Little Scholars', 'Sandy Koufax Baseball', 'Soccer Association for Youth', 'Slow Pitch Softball Team', 'T-Ball USA', 'USA Hockey Inc.', 'USA Volleyball', 'USA Water Polo', 'United States Baseball Congress', 'USSSA Baseball', 'USSSA Fast Pitch Softball', 'USSSA Slow Pitch Softball', 'US Youth Soccer Association', 'Youth Football', 'Independent Softball Association - Canada', 'Senior Softball Canada', 'USA Track & Field ', 'Boy Scouts of America', 'Girl Scouts', 'World Organization of the Scout Movement', 'Scouts Canada']
@@ -41,13 +38,8 @@ def make_url(page, search_box = "", city = "", state = "", sport = "", skill = "
     return base_url + "?" + params_url
         
 def insert_links(links, state, gender, age, sport = "", skill = "", organization = ""):
-<<<<<<< HEAD
     insert_str = ", ".join(["('%s', '%s', '%s', '%s', '%s')" % (link, state, gender, age, sport) for link in links])
     query_str = "INSERT INTO teams_sports (url, state, gender, age, sport) VALUES %s" % (insert_str)
-=======
-    insert_str = ", ".join(["('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (link, state, gender, age, sport, skill, organization) for link in links])
-    query_str = "INSERT INTO teams_all (url, state, gender, age, sport, skill, organization) VALUES %s" % (insert_str)
->>>>>>> d000e28fb6c7a6d17a294a0d473c07b694b851b1
     mysql = get_cursor()
     cur = mysql['cursor']
     cur.execute(query_str)
@@ -57,11 +49,7 @@ def insert_links(links, state, gender, age, sport = "", skill = "", organization
 def mark_stop(page_num, state, gender, age, sport = "", skill = "", organization = ""):
     mysql = get_cursor()
     cur = mysql['cursor']
-<<<<<<< HEAD
     cur.execute("INSERT INTO stops_sports (page_number, state, gender, age, sport) VALUES (%s, '%s', '%s', '%s', '%s')" % (str(page_num), state, gender, age, sport))
-=======
-    cur.execute("INSERT INTO stops_all (page_number, state, gender, age, sport, skill, organization) VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s')" % (str(page_num), state, gender, age, sport, skill, organization))
->>>>>>> d000e28fb6c7a6d17a294a0d473c07b694b851b1
     mysql['conn'].commit()
     #return cur.rowcount
 
